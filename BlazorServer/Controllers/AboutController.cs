61cdcab60c960253e78f2d7cb5101dd2c9f5092b
JOAO VICTOR MAIERU TEIXEIRA <081200025@faculdade.cefsa.edu.br>
2023-09-05 20:14:01 -0300

    Adicionar arquivos de projeto.

---
+++ BlazorServer/Controllers/AboutController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorServer.Controllers
+{
+    public class AboutController : Controller
+    {
+        [HttpGet("/api/v1/GetStatus")]
+        public async Task<IActionResult> GetStatus() => Json("The app is running");
+    }
+}
---
+++ BlazorServer/Program.cs
@@ -0,0 +1,37 @@
+using BlazorServer.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// To map controller, you need to add this line!!!
+builder.Services.AddControllers();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+// To map controller, you need to add this line!!!
+app.MapControllers();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
